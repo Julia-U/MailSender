adcab7061cd9a864cb82ccb186dbacab6407cd9a
Julia-U <uli4ka_mail@mail.ru>
2020-04-22 14:43:13 +0300

    Добавили некоторые базовые классы

---
+++ MailSender/AppConfigClass.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MailSender
+{
+    // AppConfigClass.port_smpt_server = -45;
+
+    public static class AppConfigClass
+    {
+        public static string smtp_server = "smtp.yandex.ru";
+        
+        private static int _port_smpt_server = 250;
+
+        public static int port_smpt_server 
+        {
+            get 
+            { 
+                return _port_smpt_server;
+            }
+            set 
+            {
+                if (value > 0)
+                {
+                    _port_smpt_server = value;
+                }
+                else
+                {
+                    _port_smpt_server = 1;
+                }               
+            }
+        }
+    }
+}
