4e4704e38fffec1b234e2f49b7bfa7cc81c40e9e
andrey-aka-skif <2119163@gmail.com>
2020-04-23 22:25:15 +0300

    добавлен класс отправления псьма

--- MailSender/EmailSendServiceClass.cs
+++ MailSender/EmailSendServiceClass.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
 
 namespace MailSender
 {
-    class EmailSendServiceClass
+    public class EmailSendServiceClass
     {
+        MailMessage mm;
+
+        public void MailMessage(string subject, string body, string sender, string reciver)
+        {
+            mm = new MailMessage(sender, reciver);
+            mm.Subject = subject;
+            mm.Body = body;
+            mm.IsBodyHtml = false;
+        }
+
+        public void SmtpClient()
+        {
+            SmtpClient sc = new SmtpClient(AppConfigClass.smtp_server, AppConfigClass.port_smpt_server);
+            sc.EnableSsl = true;
+            sc.DeliveryMethod = SmtpDeliveryMethod.Network;
+            sc.UseDefaultCredentials = false;
+            sc.Credentials = new NetworkCredential("отправитель@yandex.ru", "password");
+            sc.Send(mm);
+        }
     }
 }
