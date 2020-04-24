8dd3ffed7088a058c2953a64e6db7e9fce2fd072
Julia-U <uli4ka_mail@mail.ru>
2020-04-24 14:50:26 +0300

    В разработке

--- MailSender/WpfMailSender.xaml.cs
+++ MailSender/WpfMailSender.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Net;
+using System.Net.Mail;
 
 namespace MailSender
 {
@@ -29,6 +31,41 @@ namespace MailSender
             // еще один комментарий с работы
             // ещё один комментарий с github
             InitializeComponent();
+        }
+
+        private void btnSendEmail_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> listStrMails = new List<string> { "testEmail@gmail.com" };  // Список email'ов //кому мы отправляем письмо
+            string strPassword = passwordBox.Password;  // для WinForms - string strPassword = passwordBox.Text;
+            foreach (string mail in listStrMails)
+            {
+                // Используем using, чтобы гарантированно удалить объект MailMessage после использования
+                using (MailMessage mm = new MailMessage("sender@yandex.ru", mail))
+                {
+                //    // Формируем письмо
+                //    mm.Subject = "Привет из C#"; // Заголовок письма
+                //    mm.Body = "Hello, world!";       // Тело письма
+                //    mm.IsBodyHtml = false;           // Не используем html в теле письма
+                //                                     // Авторизуемся на smtp-сервере и отправляем письмо
+                //                                     // Оператор using гарантирует вызов метода Dispose, даже если при вызове 
+                //                                     // методов в объекте происходит исключение.
+                //    using (SmtpClient sc = new SmtpClient("smtp.yandex.ru", 465))
+                //    {
+                //        sc.EnableSsl = true;
+                //        sc.Credentials = new NetworkCredential("sender@yandex.ru", strPassword);
+                //        try
+                //        {
+                //            sc.Send(mm);
+                //        }
+                //        catch (Exception ex)
+                //        {
+                //            MessageBox.Show("Невозможно отправить письмо " + ex.ToString());
+                //        }
+                //    }
+                } //using (MailMessage mm = new MailMessage("sender@yandex.ru", mail))
+            }
+            SendEndWindow sew = new SendEndWindow();
+            sew.ShowDialog();                        
         }
     }
 }
